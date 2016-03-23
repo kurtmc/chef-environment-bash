653cb4a57fa6c06ae6431a477e39016b48a7789b
Kurt McAlpine <kurt.mcalpine@eroad.com>
2016-03-24 10:19:46 +1300

    Check if using production and intercept commands

---
+++ check.sh
@@ -0,0 +1,25 @@
+preexec() {
+	if [[ $1 == chef-local* ]]; then
+		return
+	fi
+
+	if [ "$(chef-show)" == "prod" ]; then
+		shopt -s extdebug
+		echo "Do you actually want to run \"$1\" in production?"
+		read response
+		if [ "$response" == "yes" ]; then
+			echo "Executing in prod"
+		else
+			echo "Command cancelled"
+			return 1
+		fi
+	fi
+	
+}
+preexec_invoke_exec () {
+	[ -n "$COMP_LINE" ] && return  # do nothing if completing
+	[ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
+	local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
+	preexec "$this_command"
+}
+trap 'preexec_invoke_exec' DEBUG
